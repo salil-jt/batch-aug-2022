58c925c79b62d075431539fb1f4b1b5bcb697112
salil-jt <saliljavatrainings@gmail.com>
2022-09-20 13:43:09 +0530

    Operators in Java

---
+++ ArithmeticOperatorsInJava.java
@@ -0,0 +1,69 @@
+class ArithmeticOperatorsInJava{
+
+
+public static void main(String[] args){
+
+
+/* Basic arithemetic operator
+System.out.println(a+b);
+System.out.println(a-b);
+System.out.println(a*b);
+System.out.println(a/b);
+*/
+
+/* modulus operators - used to get the remainder
+
+int c = a % b;
+System.out.println(c);
+*/
+
+
+/*
+// increment and decrement operator
+
+int num = 10;
+num++; // increment
+int sum = ++num;
+System.out.println("Num is :"+num); 
+System.out.println("Sum is :"+sum); 
+
+int index = 15;
+int result = --index; //decrement
+// index = index-1;
+// int result = index;
+
+
+System.out.println("Index is :"+index); 
+System.out.println("result is :"+result); 
+
+*/
+
+// Assignment Operators
+
+int a = 11;
+//a = a + 1;
+a *= 2;
+System.out.println("a is :"+a);
+
+
+// Small Program
+
+int num = 15;
+
+num += 5; // num = 20;
+
+--num; // num = 19;
+
+num -= 3; // 16
+
+num /= 2; // 8
+
+System.out.println("num is :"+num);
+
+
+
+
+
+
+}
+}
---
+++ BitWiseOperators.java
@@ -0,0 +1,32 @@
+class BitWiseOperators{
+
+
+public static void main(String[] args){
+
+/*
+int a = 10;
+int b = 3;
+
+System.out.println(a & b);
+System.out.println(a | b);
+System.out.println(a ^ b);
+
+*/
+
+
+int num = 67;
+
+// right shift >>
+
+System.out.println(num >> 1); 
+// 32 >> 1 -> 16
+// 32 >> 2 -> 8
+// 32 >> 3 -> 4
+
+
+System.out.println(32 << 2);
+
+
+
+}
+}
